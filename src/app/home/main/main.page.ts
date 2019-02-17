0465e71c849ad6270fae69f0ab24e1230e0f024e
Raul <raulibpe@gmail.com>
2019-02-17 16:56:58 +0100

    Added routerLinkActive

--- src/app/home/main/main.page.ts
+++ src/app/home/main/main.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {MenuController} from '@ionic/angular';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-main',
@@ -11,15 +12,18 @@ export class MainPage implements OnInit {
   public pages: {title: string, component: string, openTab?: any}[] = [];
   public rootPage = 'profile';
 
-  constructor(private menu: MenuController) { }
+  constructor(private menu: MenuController, private router: Router) { }
 
   ngOnInit() {
   }
 
+  onCloseMenu(): void {
+    this.menu.close('custom');
+  }
+
   onOpenMenu(): void {
-    this.menu.enable(true, 'custom');
-    this.menu.open('custom').then(x => {
-      console.log(x);
+    this.menu.enable(true, 'custom').then(x => {
+        this.menu.open('custom');
     });
   }
 
